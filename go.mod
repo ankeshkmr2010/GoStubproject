1100dde40a6a63f07ae07f169c93f785ec20cff1
ankeshkmr2010 <ankeshkmr2010@gmail.com>
2025-07-30 21:56:41 +0530

    updated pk first iteration db accessor

--- go.mod
+++ go.mod
@@ -6,7 +6,6 @@ require (
 	github.com/gin-gonic/gin v1.10.1
 	github.com/golang-migrate/migrate/v4 v4.18.3
 	github.com/google/uuid v1.6.0
-	github.com/jackc/pgtype v1.14.0
 	github.com/jackc/pgx/v5 v5.7.5
 	github.com/joho/godotenv v1.5.1
 	github.com/lib/pq v1.10.9
@@ -22,7 +21,6 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.27.0 // indirect
 	github.com/goccy/go-json v0.10.5 // indirect
-	github.com/gofrs/uuid v4.0.0+incompatible // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
@@ -32,6 +30,7 @@ require (
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgproto3/v2 v2.3.3 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/pgtype v1.14.0 // indirect
 	github.com/jackc/pgx/v4 v4.18.2 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
---
+++ repos/tasksrepo/tasksDbRepo.go
@@ -0,0 +1,219 @@
+package tasksrepo
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"gostubproject/stub/interfaces"
+	"gostubproject/stub/models/dtos"
+	s "gostubproject/stub/models/schemas"
+	"log"
+)
+
+const (
+	getLatestActiveTaskById = ` SELECT id, name, description, status, priority, created_by,is_deleted,version, task_data,created_at,updated_at
+		FROM tasks
+		WHERE id = $1
+		  AND is_deleted = false
+		ORDER BY version DESC
+		LIMIT 1;`
+
+	getLatestActiveTasksForCreator = ` SELECT DISTINCT ON (id) id, name, description, status, priority, created_by, is_deleted, version,task_data,created_at,updated_at
+	FROM tasks
+	WHERE created_by = $1
+	  AND is_deleted = false
+	ORDER BY id, version DESC;
+	`
+
+	insertIntoTasks = `INSERT INTO tasks (id, name, description, status, priority, created_by, is_deleted, version, task_data, created_at, updated_at)
+    	VALUES ($1, $2, $3, $4, $5, $6, false, 1, $7, NOW(), NOW()) `
+
+	addChildTask = `INSERT INTO parent_child_tasks (parent_task_id, child_task_id)
+    	VALUES ($1, $2)`
+
+	updateTask = `UPDATE tasks
+	SET name = $1, description = $2, status = $3, priority = $4, task_data = $5, updated_at = NOW(),version = version + 1
+	WHERE id = $6 and 
+	    is_deleted = false`
+
+	deleteTask = `UPDATE tasks
+	SET is_deleted = true, updated_at = NOW()
+	WHERE id = $1
+	  AND is_deleted = false;`
+)
+
+type TasksDbAccessorImpl struct {
+	db *pgxpool.Pool
+}
+
+func (t *TasksDbAccessorImpl) GetTaskByID(ctx context.Context, id uuid.UUID) (dtos.GetTaskResp, error) {
+	var taskData s.Task
+	row := t.db.QueryRow(ctx, getLatestActiveTaskById, id)
+	err := row.Scan(&taskData.ID, &taskData.Name, &taskData.Description, &taskData.Status, &taskData.Priority, &taskData.CreatedBy, &taskData.IsDeleted, &taskData.Version, &taskData.TaskData, &taskData.CreatedAt, &taskData.UpdatedAt)
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			log.Println("No task found with ID: ", id)
+			return dtos.GetTaskResp{}, nil
+		}
+		return dtos.GetTaskResp{}, err
+	}
+
+	return dtos.GetTaskResp{
+		NonDeletedTaskResp: &dtos.NonDeletedTaskResp{
+			ID:          taskData.ID,
+			Version:     taskData.Version,
+			Name:        taskData.Name,
+			Description: taskData.Description,
+			Status:      taskData.Status,
+			Priority:    taskData.Priority,
+			CreatedBy:   taskData.CreatedBy,
+			TaskData:    taskData.TaskData,
+			CreatedAt:   taskData.CreatedAt,
+			UpdatedAt:   taskData.UpdatedAt,
+			Children:    nil, // Assuming children are not fetched here, can be added later
+		},
+	}, nil
+}
+
+func (t *TasksDbAccessorImpl) GetTasksForCreator(ctx context.Context, createdBy uuid.UUID) ([]dtos.GetTaskResp, error) {
+	var tasks []dtos.GetTaskResp
+	rows, err := t.db.Query(ctx, getLatestActiveTasksForCreator, createdBy)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var taskData s.Task
+		if err := rows.Scan(&taskData.ID, &taskData.Name, &taskData.Description, &taskData.Status, &taskData.Priority, &taskData.CreatedBy, &taskData.IsDeleted, &taskData.Version, &taskData.TaskData, &taskData.CreatedAt, &taskData.UpdatedAt); err != nil {
+			return nil, err
+		}
+
+		tasks = append(tasks, dtos.GetTaskResp{
+			NonDeletedTaskResp: &dtos.NonDeletedTaskResp{
+				ID:          taskData.ID,
+				Version:     taskData.Version,
+				Name:        taskData.Name,
+				Description: taskData.Description,
+				Status:      taskData.Status,
+				Priority:    taskData.Priority,
+				CreatedBy:   taskData.CreatedBy,
+				TaskData:    taskData.TaskData,
+				CreatedAt:   taskData.CreatedAt,
+				UpdatedAt:   taskData.UpdatedAt,
+				Children:    nil, // Assuming children are not fetched here, can be added later
+			},
+			IsDeleted: taskData.IsDeleted,
+		})
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return tasks, nil
+}
+
+func (t *TasksDbAccessorImpl) CreateTask(ctx context.Context, task dtos.CreateTaskReq) (dtos.CreateTaskResp, error) {
+	id := uuid.New()
+	_, err := t.db.Exec(ctx, insertIntoTasks, id, task.Name, task.Description, task.Status, task.Priority, task.CreatedBy, task.TaskData)
+	if err != nil {
+		log.Println("Error inserting new task: ", err)
+		return dtos.CreateTaskResp{}, err
+	}
+
+	return dtos.CreateTaskResp{
+		ID: id,
+		GetTaskResp: &dtos.GetTaskResp{
+			NonDeletedTaskResp: &dtos.NonDeletedTaskResp{
+				ID:          id,
+				Name:        task.Name,
+				Description: task.Description,
+				Status:      task.Status,
+				Priority:    task.Priority,
+				CreatedBy:   task.CreatedBy,
+				TaskData:    task.TaskData,
+			},
+		},
+	}, nil
+}
+
+func (t *TasksDbAccessorImpl) AddNewChildTask(ctx context.Context, task dtos.AddNewChildTaskReq) error {
+	if len(task.ChildTask) == 0 {
+		return errors.New("no child tasks provided")
+	}
+
+	//  change this to bulk inserts
+	for _, childID := range task.ChildTask {
+		_, err := t.db.Exec(ctx, addChildTask, task.TaskID, childID)
+		if err != nil {
+			log.Println("Error adding child task: ", err)
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (t *TasksDbAccessorImpl) UpdateTask(ctx context.Context, task dtos.UpdateTaskReq) (dtos.CreateTaskResp, error) {
+	tx, err := t.db.BeginTx(ctx, pgx.TxOptions{})
+	if err != nil {
+		return dtos.CreateTaskResp{}, fmt.Errorf("begin tx: %w", err)
+	}
+	defer func(tx pgx.Tx, ctx context.Context) {
+		err := tx.Rollback(ctx)
+		if err != nil {
+			log.Println("Rollback failed: ", err)
+		}
+	}(tx, ctx)
+
+	_, err = tx.Exec(ctx, updateTask, task.Name, task.Description, task.Status, task.Priority, task.TaskData, task.TaskID)
+	if err != nil {
+		log.Println("Error updating task: ", err)
+		return dtos.CreateTaskResp{}, err
+	}
+
+	return dtos.CreateTaskResp{
+		ID: task.TaskID,
+		GetTaskResp: &dtos.GetTaskResp{
+			NonDeletedTaskResp: &dtos.NonDeletedTaskResp{
+				ID:          task.TaskID,
+				Name:        task.Name,
+				Description: task.Description,
+				Status:      task.Status,
+				Priority:    task.Priority,
+				TaskData:    task.TaskData,
+			},
+		},
+	}, nil
+}
+
+func (t *TasksDbAccessorImpl) DeleteTask(ctx context.Context, taskID uuid.UUID) error {
+	tx, err := t.db.BeginTx(ctx, pgx.TxOptions{})
+	if err != nil {
+		return fmt.Errorf("begin tx: %w", err)
+	}
+	defer func(tx pgx.Tx, ctx context.Context) {
+		err := tx.Rollback(ctx)
+		if err != nil {
+			log.Println("Rollback failed: ", err)
+		}
+	}(tx, ctx)
+
+	_, err = tx.Exec(ctx, deleteTask, taskID)
+	if err != nil {
+		log.Println("Error deleting task: ", err)
+		return err
+	}
+
+	return tx.Commit(ctx)
+}
+
+func NewTaskDbAccessorImpl(db *pgxpool.Pool) interfaces.TaskDbAccessor {
+	return &TasksDbAccessorImpl{
+		db: db,
+	}
+}
